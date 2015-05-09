f319ae0a6ee14f68e881a6c0e1870dcf8b5b632e
Olivier Droog <olivier1980@gmail.com>
2015-05-09 12:15:28 +0200

    Fix example

--- examples/simpleApp/index.js
+++ examples/simpleApp/index.js
@@ -5,14 +5,15 @@ var http = require('http');
 
 var app = express();
 
+//Include router-related middleware before yaml-router
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 require('yaml-router')(app);
 
 // view engine setup
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'twig');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 var server = http.createServer(app);
 server.listen(8080);
