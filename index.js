9b57a6edf16b7cc976463dd4c591b6be5e3e61f3
Olivier Droog <olivier1980@gmail.com>
2015-05-14 23:17:38 +0200

    automatic code reformatting

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ var path = require('path');
 module.exports = function(app, options) {
 
 	options = extend(true, {
-		routingFile: __dirname+'/../../app/config/routes.yml',
-		controllerPath: __dirname+'/../../app/controllers',
+		routingFile: __dirname + '/../../app/config/routes.yml',
+		controllerPath: __dirname + '/../../app/controllers',
 		helperName: 'url'
 	}, options || {});
-	
-	expressReverse(app, {helperName: options.helperName});
+
+	expressReverse(app, {
+		helperName: options.helperName
+	});
 
 	initRoutes(app, options);
 };
@@ -26,18 +28,18 @@ function initRoutes(app, options) {
 	var controllers = {};
 	fs.readdirSync(options.controllerPath).forEach(function(file) {
 		var name = file.substr(0, file.indexOf('.'));
-		controllers[name] = require(options.controllerPath +'/'+ file);
+		controllers[name] = require(options.controllerPath + '/' + file);
 	});
 
 
 	var parseYML = function(file, prefix) {
-		
+
 		prefix = prefix || '';
-		
+
 		var routesYaml = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-		
+
 		for (var key in routesYaml) {
-			
+
 			var obj = routesYaml[key];
 
 			if (obj.resource) {
@@ -48,14 +50,14 @@ function initRoutes(app, options) {
 			var split = obj.controller.split(':'),
 				bundle = split[0],
 				controller = split[1] + 'Controller';
-		 
+
 			obj.methods.forEach(function(method) {
 				var c = controllers[bundle]();
-				app[method.toLowerCase()](key, prefix + obj.pattern, c[controller] );
+				app[method.toLowerCase()](key, prefix + obj.pattern, c[controller]);
 			});
 		}
 	};
-	
+
 	//Load YAML
 	parseYML(options.routingFile);
 
