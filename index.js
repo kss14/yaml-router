675dcbd1e617969875362e4e53c30292664bbf65
kevisain <kevin.saint-sauveur@partnering.fr>
2019-11-15 14:45:44 +0100

    module pimped for my project

--- index.js
+++ index.js
@@ -1,77 +1,112 @@
-var extend = require('node.extend');
-var yaml = require('js-yaml');
-var fs = require('fs');
-var expressReverse = require('express-reverse');
-var path = require('path');
+const extend         = require('node.extend'),
+      yaml           = require('js-yaml'),
+      fs             = require('fs'),
+      expressReverse = require('express-reverse'),
+      path           = require('path'),
+      Express        = require('express')
 
-module.exports = function(app, options) {
-
+module.exports = function (app, options)
+{
 	options = extend(true, {
-		routingFile: __dirname + '/../../app/config/routes.yml',
+		routingFile   : __dirname + '/../../app/config/routes.yml',
 		controllerPath: __dirname + '/../../app/controllers',
-		helperName: 'url'
-	}, options || {});
-
+		helperName    : 'url'
+	}, options || {})
+	
 	expressReverse(app, {
 		helperName: options.helperName
-	});
+	})
+	
+	initRoutes(app, options)
+}
 
-	initRoutes(app, options);
-};
-
-function initRoutes(app, options) {
-
-	// Load all controllers
-	//TODO: 
-	//-search subfolders
-
-	var controllers = {};
-	fs.readdirSync(options.controllerPath)
-		.filter(function(file){
-			return file.indexOf('test.js') === -1 && file.indexOf('spec.js') === -1
+function initRoutes (app, options) {
+	let controllers = []
+	let i           = 0
+	
+	options.controllerPath.forEach(function (dir)
+	{
+		fs.readdirSync(dir).forEach(function (file)
+		{
+			let name = file.substr(0, file.indexOf('.'))
+			controllers.push(require(dir + '/' + file))
 		})
-		.forEach(function(file) {
-			var name = file.substr(0, file.indexOf('.'));
-			controllers[name] = require(options.controllerPath + '/' + file);
-		});
-
-
-	var parseYML = function(file, prefix) {
-
-		prefix = prefix || '';
-
-		var routesYaml = yaml.safeLoad(fs.readFileSync(file, 'utf8'));
-
-		for (var key in routesYaml) {
-
-			var obj = routesYaml[key];
-
+	})
+	
+	let parseYML = function (file, prefix)
+	{
+		let routesYaml = yaml.safeLoad(fs.readFileSync(file, 'utf8'))
+		
+		prefix = prefix || ''
+		
+		for (let key in routesYaml) {
+			const obj = routesYaml[key]
+			
 			if (obj.resource) {
-				parseYML(path.dirname(file) + '/' + obj.resource, obj.prefix);
-				continue;
+				parseYML(path.dirname(file) + '/' + obj.resource, obj.prefix)
+				continue
 			}
-
-			var split = obj.controller.split(':'),
-				bundle = split[0],
-				controller = split[1] + 'Controller';
-
+			
+			const reg               = /^[A-Z][a-zA-Z]*(\.[A-Z][a-zA-Z]*)*$/g
+			// controller parameter format "Bundle:Controller:action"
+			let split               = obj.controller.split(':').reverse(),
+			    action              = split[0] + 'Action',
+			    controllerClassName = split[1] + 'Controller',
+			    namespace           = split[2] !== undefined ? split[2] : null
+			
+			if (!reg.test(namespace)) {
+				throw new Error('bad format namespace "' + namespace + '" ex: Foo or Foo.Bar')
+			}
+			
+			if (!fs.existsSync(__dirname + '/../../dist/' + namespace.replace('.', '/'))) {
+				throw new Error('directory "dist/src/' + namespace.replace('.', '/') + '" doesn\'t exit.')
+			}
+			
 			if (!obj.methods) {
-				throw new Error('No methods defined for controller ' + obj.controller);
+				throw new Error('No methods defined for controller ' + obj.controller)
 			}
-
-			obj.methods.forEach(function(method) {
-				var c = controllers[bundle]();
-				var controllerFunction = c[controller];
-				if (!controllerFunction) {
-					throw new Error('No controller found for ' + obj.controller);
+			
+			obj.methods.forEach(function (method)
+			{
+				console.log(fs.existsSync(__dirname + '/../../dist/src/' + namespace.replace('.', '/')))
+				console.log('START => ' + i + '  ' + controllerClassName)
+				console.log(namespace + ' => ' + Object.byString(controllers[i], namespace))
+				
+				if (Object.byString(controllers[i], namespace) === undefined) {
+					throw new Error('namespace "' + namespace + '" not found.')
 				}
 				
-				app[method.toLowerCase()](key, prefix + obj.pattern, controllerFunction);
-			});
+				let objBuild       = Object.byString(controllers[i], namespace + '.' + controllerClassName)
+				let controller     = new objBuild
+				let actionFunction = controller[action]
+				
+				console.log('END ' + controllerClassName)
+				if (!actionFunction) {
+					throw new Error('No controller found for ' + obj.controller)
+				}
+				
+				app[method.toLowerCase()](key, prefix + obj.pattern, actionFunction)
+			})
+			
+			i += 1
 		}
-	};
-
+	}
 	//Load YAML
-	parseYML(options.routingFile);
+	parseYML(options.routingFile)
+}
 
+Object.byString = function (o, s)
+{
+	s     = s.replace(/\[(\w+)\]/g, '.$1') // convert indexes to properties
+	s     = s.replace(/^\./, '')           // strip a leading dot
+	var a = s.split('.')
+	for (var i = 0, n = a.length; i < n; ++i) {
+		var k = a[i]
+		if (k in o) {
+			o = o[k]
+		} else {
+			return
+		}
+	}
+	return o
 }
